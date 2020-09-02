1af627ede46e3a78637d1eb47eef03659d6eb9d0
nowaday <nowaday@github.com>
2020-09-02 21:20:49 +0300

    start do parser

--- common.py
+++ common.py
@@ -42,3 +42,37 @@ def createLogger(name, lvl=logging.DEBUG):
 # print(process_query(s2))
 # print(process_query(s3))
 # a = process_query(s4)
+
+
+
+# root ::= value
+# value ::= string | filter | '&'
+# filter ::= string '=' value
+
+def parse_filter(inp):
+    filter_regex = re.compile(r'(\w+)\s*=\s*([a-zA-Z0-9_ ]*)\s*(.*)')
+    match = filter_regex.search(inp)
+    if match is not None:
+        field, value, remainds = match.groups()
+        return field.strip(), value.strip(), remainds
+
+
+def parse(s):
+    inp = s
+    result = []
+    while inp:
+        field, value, inp = list(parse_filter(inp))
+        result.append([field, value])
+    return result
+
+print(parse('level=1 & ritual=True'))
+
+print(parse('level=1 & ritual=True'))
+print(parse('level= 1& ritual= True'))
+print(parse('level = 1 & ritual= True'))
+print(parse('level =1'))
+print(parse('level   =1'))
+print(parse('name = acid arrow & ritual=True'))
+
+
+
